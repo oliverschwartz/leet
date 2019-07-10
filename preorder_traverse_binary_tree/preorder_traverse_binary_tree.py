8b51b9499d5e230075c080ac8c045d84f512e90b
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-07-10 14:15:54 +1000

    preorder traverse binary tree

---
+++ preorder_traverse_binary_tree/preorder_traverse_binary_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        
+        # we know that given an element as root, the smaller elements will be
+        # in the left subtree, while the larger elements will be in the right subtree
+        #
+        # this lets us write this thing recursively
+        #
+        def recursePreorder(preorder):
+            root = TreeNode(preorder.pop(0))
+            if preorder == []: return root
+            
+            else:
+                left_preorder, right_preorder = [], []
+                for n in preorder:
+                    if n < root.val: left_preorder.append(n)
+                    else: right_preorder.append(n)
+                if left_preorder != []: root.left = recursePreorder(left_preorder)
+                if right_preorder != []: root.right = recursePreorder(right_preorder)
+            return root
+        return recursePreorder(preorder)
