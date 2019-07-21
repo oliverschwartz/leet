95994496c6150d74a14627f48f0d174eec814bb4
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-07-21 21:08:10 +1000

    reverse linked list

--- ctci/reverse_linked_list.py
+++ ctci/reverse_linked_list.py
@@ -4,6 +4,8 @@
 #         self.val = x
 #         self.next = None
 
+"""Recursive solution"""
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         if not head: return None
@@ -20,3 +22,30 @@ class Solution:
         while cur.next is not None: cur = cur.next
         recurseReverseList(head)
         return cur
+
+
+"""Iterative, form entirely new list"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return None
+        
+        stack = []
+        cur = head
+        while cur is not None:
+            stack.append(cur.val)
+            cur = cur.next
+       
+        new_head = ListNode(stack.pop())
+        cur = new_head
+        while stack:
+            cur.next = ListNode(stack.pop())
+            cur = cur.next
+        return new_head
+            
