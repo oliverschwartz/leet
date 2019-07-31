9eb8021d40fbd36c9dff843f70a0cdfc6d84fd0e
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-07-31 16:10:05 +1000

    single elt sorted arr

---
+++ single_elt_sorted_arr/single_elt_sorted_arr.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        
+        # annoying edge case
+        if n == 1: return nums[0]
+        
+        l = 0
+        r = n - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            # condition where we've found the single number, including edge cases
+            if (
+                mid == 0 and nums[mid] != nums[mid + 1] or
+                mid == n - 1 and nums[mid] != nums[mid - 1] or
+                nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]
+            ):
+                return nums[mid]
+            
+            # case where we need to move right. In this case, for a pair of equal
+            # elements, the left one is at an even index while the right one is at an
+            # odd index. Note we could be sitting at either one
+            elif (
+                mid % 2 == 0 and nums[mid] == nums[mid + 1] or
+                mid % 2 == 1 and nums[mid] != nums[mid + 1]
+            ):
+                l = mid + 1
+                
+            else:
+                r = mid - 1
+            
+        return None
+        
