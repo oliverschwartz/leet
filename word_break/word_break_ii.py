362a02ead532cdcd617fcc1f4195b77c93a0c5d9
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-08-05 10:16:43 +1000

    memoized

--- word_break/word_break_ii.py
+++ word_break/word_break_ii.py
@@ -27,3 +27,38 @@ class Solution:
         
         recurseBreak(0, '')
         return [x for x in self.ans]
+
+
+"""memoized version"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+
+        n = len(s)
+        
+        # can you wordBreak s[idx => end]?
+        memo = {}
+        def recurseBreak(idx):
+          
+            if idx in memo:
+                return memo[idx]
+            
+            memo[idx] = []
+            
+            if idx == n:
+                memo[idx] = ['']
+                return memo[idx]
+            
+            # otherwise, walk up the string.
+            # Try a new split wherever possible
+            end = idx
+            while end < n:
+                if s[idx : end + 1] in wordDict:
+                    for tail in recurseBreak(end + 1):
+                        new = s[idx : end + 1]
+                        if tail != '':
+                            new += (' ' + tail)
+                        memo[idx].append(new)
+                end += 1
+            return memo[idx]
+        
+        return recurseBreak(0)
